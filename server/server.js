0408b1e7af33a6927de70b8c9a2ee5ccca93e4ad
kowsalya L <kowsilogu03@gmail.com>
2025-04-26 16:39:09 +0530

    hole project changed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/connectDB.js'
 import authRoute from './routes/authRoute.js'
@@ -13,14 +12,13 @@ const __dirname = path.resolve()
 const app = express()
 connectDB()
 
-app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoute)
 app.use('/api/user',userRoute)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/client/dist")));
 }
 
 app.listen(process.env.PORT,()=>{
